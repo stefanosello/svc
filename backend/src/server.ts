09ef24cde9093353d3853200075a4715b128c135
Davide Volpe <volpe.davide.vd@gmail.com>
2022-03-18 11:24:43 +0100

    Implemented communication based on Redis SMQ
    Co-authored-by: Stefano Sello <ssello1@users.noreply.github.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,22 @@
 import express from 'express';
-import axios from 'axios';
 import bodyParser from 'body-parser'
 import path from 'path';
 import fs from 'fs';
-
-const instance = axios.create({
-  baseURL: 'http://svc-compiler:1337/',
-})
+import RedisSMQ from "rsmq";
 
 const base_dir = "/src";
+const rsmq = new RedisSMQ( {host: "svc-smq", port: 6379, ns: "rsmq", realtime: true} );
+
+rsmq.createQueue({ qname: "myqueue" }, function (err, resp) {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  if (resp === 1) {
+    console.log("queue created")
+  }
+});
 
 async function doRequest(filename: string) {
   const fullPath = path.join(base_dir, filename);
@@ -16,11 +24,25 @@ async function doRequest(filename: string) {
     throw new Error("File does not exists!");
   }
 
-  const res = await instance.post("/compile", { filename } );
+  const payload = {
+    filename: filename,
+  };
+
+  console.log("Sending request for", payload);
+  rsmq.sendMessage({ qname: "myqueue", message: JSON.stringify(payload) }, function (err, resp) {
+    if (err) { 
+      console.error(err)
+      return
+    }
   
+    console.log("Message sent. ID:", resp);
+  });
+  
+  /*
   console.log(`[INFO] exit code: ${res.data.exitCode}`);
   console.log(`[INFO] stdout:\n${res.data.stdout}`);
   console.log(`[INFO] stderr:\n${res.data.stderr}`);
+  */
 }
 
 const app = express();
@@ -35,9 +57,17 @@ app.get("/", (req, res) => {
   res.status(200).send("<h1>Backend works!</h1>");
 });
 
+app.post("/compilation-result", (req, res) => {
+
+  console.log(req.body.data);
+
+});
+
 app.listen(80, () => {
   console.log(`[INFO] Server running on port 80`);
-  
-  doRequest('tests/pass.cpp');
-  doRequest('tests/fail.cpp');
+
+  setTimeout( () => {
+    doRequest('tests/fail.cpp');
+    doRequest('tests/pass.cpp');
+  }, 3000);
 });
--- compiler/src/compiler.ts
+++ compiler/src/compiler.ts
@@ -1,13 +1,26 @@
 import express from 'express';
-
 import { spawnSync } from 'child_process';
 import shlex from 'shlex';
 import path from 'path';
 import bodyParser from 'body-parser'
 import fs from 'fs';
+import RSMQWorker from "rsmq-worker";
+import RedisSMQ, { QueueMessage }  from "rsmq";
+import axios from 'axios';
 
-const app = express();
 const base_dir = "/src";
+
+const backendInstance = axios.create({
+  baseURL: 'http://svc-backend:80/',
+})
+
+var options = {
+  host: "svc-smq",
+  port: 6379
+};
+
+const rsmq = new RedisSMQ( options );
+const worker = new RSMQWorker( "myqueue", { ...options, autostart: true } );
 
 function compile(inPath: string, outPath: string) {
   const cmdStr = `g++ -std=c++17 ${inPath} -o ${outPath}` ;
@@ -27,6 +40,8 @@ function rmFile(filePath: string) {
   fs.unlinkSync(filePath);
 }
 
+const app = express();
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -36,7 +51,7 @@ app.use(bodyParser.json())
 app.get("/", (req, res) => {
   res.status(500).send("<h1>Compiler shouldn't work!</h1>");
 });
-
+/*
 app.post("/compile", (req, res) => {
   const inFilename = req.body.filename;
   const inPath = path.join(base_dir, inFilename);
@@ -60,7 +75,69 @@ app.post("/compile", (req, res) => {
 
   res.status(200).send(data);
 });
-
+*/
 app.listen(1337, () => {
   console.log(`[INFO] Compiler service running on port 1337`);
+
+/*
+  rsmq.createQueue({ qname: "myqueue" }, function (err, resp) {
+    if (err) {
+      console.error(err)
+      return
+    }
+  
+    if (resp === 1) {
+      console.log("queue created")
+    }
+  });
+*/
+  worker.on( "message", ( msg: string, next: Function, id: string ) => {  
+    
+    if (id) {
+      console.log("Message received and deleted from queue", id)
+      
+      // parse message and compile
+      const payload = JSON.parse(msg);
+      const inFilename: string = payload.filename;
+      const inPath: string = path.join(base_dir, inFilename);
+    
+      const outFilename: string = `${inFilename}.out`;
+      const outPath: string = path.join(base_dir, outFilename);
+    
+      const process = compile(inPath, outPath);
+    
+      console.log(`[COMPILER] Compilation terminated with status ${process.status}`);
+      
+      if (process.status == 0) {
+        rmFile(outPath);
+      }
+      
+      const data = {
+        exitCode: process.status,
+        stdout: process.stdout.toLocaleString(),
+        stderr: process.stderr.toLocaleString(),
+      };
+
+      // TODO: gestire risposta?
+      backendInstance.post("/compilation-result", { data });
+
+
+      rsmq.deleteMessage({ qname: "myqueue", id }, function (err, resp) {
+        if (err) {
+          console.error(err)
+          return
+        }
+      
+        if (resp === 1) {
+          console.log("Message deleted.")
+        } else {
+          console.log("Message not found.")
+        }
+      });
+
+    } else {
+      console.log("No messages for me...")
+    }
+  });
+
 });
