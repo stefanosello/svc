a970f02f5e36245a7dadbcdab63f9742232741ad
Stefano Sello <sellostefano@gmail.com>
2022-03-19 12:34:21 +0100

    Removed unused source code

--- shared/24c670f6-81e5-421b-957c-ca54957b54ed.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-using namespace std;
-int main() {
-  cout << "Hello world";
-  return 0;
-}
--- shared/f3f099fb-8858-4bbb-868e-d96327f77aec.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-  std::cout << "Hello world";
-  return 0;
-}
