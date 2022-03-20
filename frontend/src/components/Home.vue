2cbb24ce24768279b63673c5ec1503ed722de7f9
Davide Volpe <volpe.davide.vd@gmail.com>
2022-03-20 14:56:18 +0100

    Added loading circle during compilation

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="container h-100vh d-flex flex-column align-items-center justify-content-center">
-    <h1> SVC </h1>
+  <div class="container h-100vh">
+    <h1 class="d-flex my-5 justify-content-center"> SVC </h1>
 
-    <div class="w-100 my-5">
-      <label for="codeInput" class="form-label">Write your c++ code here</label>
-      <prism-editor id="codeInput" class="my-editor" v-model="code" :highlight="highlight" line-numbers></prism-editor>
-    </div>
-    <div class="w-100 d-flex justify-content-end input-group mb-3">
-      <input type="text" class="form-control" v-model="cflags" placeholder="Compilation flags">
-      <div class="input-group-append">
-        <button class="btn btn-secondary" @click="compile">Compile</button>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+      <div class="w-100 my-5">
+        <label for="codeInput" class="form-label">Write your c++ code here</label>
+        <prism-editor id="codeInput" class="my-editor" v-model="code" :highlight="highlight" line-numbers></prism-editor>
+      </div>
+      <div class="w-100 d-flex justify-content-end input-group mb-3">
+        <input type="text" class="form-control" v-model="cflags" placeholder="Compilation flags">
+        <div class="input-group-append">
+          <button class="btn btn-primary" @click="compile">Compile</button>
+        </div>
+      </div>
+
+      <div class="w-100">
+        <div v-if="compilationInProgress" class="d-flex justify-content-center">
+          <Preloader color="#007BFF" scale="0.6"/>
+        </div>
+        <div v-if="!compilationInProgress && compiled">
+          <div v-bind:class="{ 'text-success': this.exitCode === 0, 'text-danger': this.exitCode !== 0,  }" >
+            {{ exitCodeDisplay }}
+          </div>
+          <div>
+            <pre>{{ errorDisplay }}</pre>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div v-if="compiled" class="w-100 my-5">
-      <div v-bind:class="{ 'text-success': this.exitCode === 0, 'text-danger': this.exitCode !== 0,  }" >
-        {{ exitCodeDisplay }}
-      </div>
-      <div>
-        <pre>{{ stderr }}</pre>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -34,11 +42,13 @@ import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/themes/prism-tomorrow.css';
+import Preloader from './Preloader.vue'
 
 export default {
   name: 'HelloWorld',
   components: {
     PrismEditor,
+    Preloader,
   },
   data() {
     return {
@@ -46,6 +56,7 @@ export default {
       exitCode: '',
       stderr: '',
       compiled: false,
+      compilationInProgress: false,
       cflags: ''
     }
   },
@@ -57,6 +68,14 @@ export default {
         return 'Complation Failed'
       }
     },
+    errorDisplay: function() {
+      if (!this.stderr)
+        return ''
+
+      const regex = /\/src\/([a-z0-9]*-?)*.cpp/gm;
+      console.log(this.stderr.match(regex));
+      return this.stderr.replaceAll(regex, '(INPUT)')
+    }
   },
   methods: {
     highlight(code) {
@@ -71,6 +90,7 @@ export default {
       )
       .then(res => {
         this.compiled = false;
+        this.compilationInProgress = true;
         console.log(res);
       });
     }
@@ -78,7 +98,9 @@ export default {
 
   mounted() {
     socketService.on("compilation-result", (results) => {
-      console.log(results);
+      //console.log(results);
+      this.compilationInProgress = false;
+
       this.exitCode = results.exitCode;
       this.stderr = results.stderr;
       this.compiled = true;
