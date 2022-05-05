43c79d572dc60c12400009bfbd74e5fb94b0056a
Stefano Sello <sellostefano@gmail.com>
2022-05-05 20:29:49 +0200

    Production docker compose configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,6 @@ import "bootstrap"
 
 import ApiService from './services/api.service'
 
-ApiService.init(`http://${process.env.VUE_APP_API_HOST}:${process.env.VUE_APP_API_PORT}/${process.env.VUE_APP_API_PATH}`)
+ApiService.init(process.env.VUE_APP_API_ROOT)
 
 createApp(App).mount('#app')
