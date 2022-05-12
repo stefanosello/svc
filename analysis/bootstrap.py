024b6912a5cbb2ff78fe9cac2ac8390099ed75c9
Davide Volpe <volpe.davide.vd@gmail.com>
2022-05-12 18:00:16 +0200

    Created script for tests! 🎉
    Co-authored-by: Stefano Sello <sellostefano@gmail.com>

---
+++ analysis/bootstrap.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+import yaml
+from yaml.loader import SafeLoader
+import subprocess
+import shlex
+import requests
+import os
+from datetime import datetime
+
+CONFIGURATION = {}
+
+def print_header():
+    print()
+    print("   $$$$$$\  $$\    $$\  $$$$$$\      ")
+    print("   $$  __$$\ $$ |   $$ |$$  __$$\    ")
+    print("   $$ /  \__|$$ |   $$ |$$ /  \__|   ")
+    print("   \$$$$$$\  \$$\  $$  |$$ |         ")
+    print("    \____$$\  \$$\$$  / $$ |         ")
+    print("   $$\   $$ |  \$$$  /  $$ |  $$\    ")
+    print("   \$$$$$$  |   \$  /   \$$$$$$  |   ")
+    print("    \______/     \_/     \______/    ")
+    print()
+    print("  SVC - Performance analysis tester  ")  
+    print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")                                              
+    print()
+
+def log(txt):
+  print(f'> {txt}')
+
+def read_yaml():
+  with open('configuration.yml') as f:
+    data = yaml.load(f, Loader=SafeLoader)
+    return data
+
+def prepare_config(config_name, config_params):
+  global CONFIGURATION
+
+  log(f"Preparing configuration {config_name}...")
+
+  ip = CONFIGURATION['tsung']['ip']
+  interarrival = config_params['interarrival']
+  max_jobs = config_params['max_jobs']
+  
+  # set ip
+  with open('ip.txt', 'w+') as f:
+    f.write(f'{ip}')
+
+  # set interarrival
+  with open('interarrival.txt', 'w+') as f:
+    f.write(f'{interarrival}')
+
+  # set max_jobs
+  url = f'http://{ip}:8080/stats/update'
+  params = { 
+    'maxJobs': max_jobs, 
+    'resetMaxJobs': 1 
+  }
+  r = requests.get(url, params = params)
+  if r.status_code != 200:
+    raise Exception("cannot update server stats")
+
+def run_all_configs():
+  global CONFIGURATION
+
+  configs = list(CONFIGURATION['tsung']['configs'].keys())
+  for config_name in configs:
+    run_config(config_name)
+
+def run_config(config_name):
+  global CONFIGURATION
+  
+  configuration = CONFIGURATION['tsung']['configs'][config_name]
+  prepare_config(config_name, configuration['params'])
+  run_tsung()
+
+def run_tsung():
+  global CONFIGURATION
+  
+  config_file = CONFIGURATION['tsung']['config_file']
+  log_dir = CONFIGURATION['tsung']['log_dir']
+
+  now = datetime.now()
+  dir_name = now.strftime("%Y%m%d-%H%M")
+  
+  log(f"Running Tsung...")
+  cmd = f"tsung -f {config_file} -l {log_dir} start"
+  process = subprocess.Popen(shlex.split(cmd))
+  process.wait()
+  
+  log(f"Running Tsung-stats...")
+  dir_path = os.path.join(log_dir, dir_name)
+  os.chdir(f'./{dir_path}')
+  cmd = "perl /opt/tsung-1.7.0/src/tsung_stats.pl"
+  process = subprocess.Popen(shlex.split(cmd))
+  process.wait()
+
+
+if __name__=="__main__":
+  print_header()
+  CONFIGURATION = read_yaml()
+
+  run_config("test1")
+
+  # run_all_configs()
