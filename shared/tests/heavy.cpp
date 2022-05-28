d92c7e44fb82a3f2289550cf819a37c02d9b5b03
Stefano Sello <sellostefano@gmail.com>
2022-05-28 18:12:20 +0200

    Fixed c code

--- shared/tests/heavy.cpp
+++ shared/tests/heavy.cpp
@@ -1,26 +1,18 @@
-template <int i>
-class A
-{
-    A<i - 1> x;
-    A<i - 2> y;
+template < int i > class  A                           
+{                                  
+   A<i-1> x;                     
+   A<i-2> y;
 };
 
-template <>
-class A<0>
-{
-    char a;
-};
+template <> class A<0>             
+{ char a; };
 
-template <>
-class A<1>
-{
-    char a;
-};
+template <> class A<1>             
+{ char a; };
 
-int main()
-{
-    A<35> b;
-    return 0;
+int main() {
+  A<35>  b;
+  return 0;
 }
 
-//
+// template &lt;int i&gt; class A { A&lt;i-1&gt; x; A&lt;i-2&gt; y;};template &lt;&gt; class A&lt;0&gt; { char a; };template &lt;&gt; class A&lt;1&gt; { char a; };int main() {  A&lt;35&gt;  b;  return 0;}
